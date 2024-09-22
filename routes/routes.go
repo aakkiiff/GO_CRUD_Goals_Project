3efbb425ad0258f018c90d79e405ec3252c6ffde
Ahsan Sourav <aasourav@yahoo.com>
2024-09-22 10:33:21 +0600

    rest with mongo

--- routes/routes.go
+++ routes/routes.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/hello", hello)
 	server.POST("/create-goal", createGoal)
-	server.POST("/edit-goal", editGoals)
+	server.PUT("/edit-goal", editGoals)
 	server.DELETE("/delete-goal", deleteGoals)
 	server.GET("/goals", getGoals)
 }
