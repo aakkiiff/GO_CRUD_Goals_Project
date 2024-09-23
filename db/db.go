f98626af3cb968977bc0c8e320f793d94d858693
akif <jackakif@gmail.com>
2024-09-23 23:52:52 +0600

    variable input added

--- db/db.go
+++ db/db.go
@@ -2,14 +2,26 @@ package db
 
 import (
 	"log"
+	"os"
 
 	"github.com/kamva/mgm/v3"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func InitMongoDB() {
+	// Retrieve environment variables
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+
+	// Check if environment variables are set
+	if username == "" || password == "" {
+		log.Fatal("MongoDB credentials are not set!")
+	}
+
 	// Setup the mgm default config
-	err := mgm.SetDefaultConfig(nil, "goals", options.Client().ApplyURI("mongodb+srv://akif:akif@cluster0.61sku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsInsecure=true"))
+	err := mgm.SetDefaultConfig(nil, "goals", options.Client().ApplyURI(
+		"mongodb+srv://"+username+":"+password+"@cluster0.61sku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsInsecure=true",
+	))
 	if err != nil {
 		panic("could not connect to the database!")
 	}
